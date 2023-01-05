e66db22acaa8e84cbf444ab4db3317214b62bcf5
Md.Fahim Rahman <114246825+mdfahimrahman2020@users.noreply.github.com>
2023-01-05 19:25:13 +0600

    Add files via upload

---
+++ math.java
@@ -0,0 +1,41 @@
+
+public class math{
+public static void main(String[] args){
+	
+
+int a = 45;
+int x = 2;
+int y = 3;
+double z = 12.6;
+
+int absolute = Math.abs(y);
+System.out.print("\n Absolute value of 3rd Number"+absolute);
+
+
+
+int max = Math.max(x,y);
+System.out.print("\nThe maximum value is : "+max);
+
+int min = Math.min(x,y);
+System.out.print("\nThe minimum value is : "+min);
+
+double power = Math.pow(x,y);
+System.out.print("\nX to the power Y : "+power);
+
+double rnd = Math.round(z);
+System.out.print("\nRound value : "+rnd);
+
+double trigonometric = Math.sin(a);
+System.out.print("\n Sin :"+trigonometric);
+
+double trigonometric1 = Math.tan(a);
+System.out.print("\n Tan :"+trigonometric1);
+
+double pi = Math.PI;
+System.out.print("\n The valu of PI is : "+pi);
+
+
+	
+	
+}
+}
